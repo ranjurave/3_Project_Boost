96cd0cc67a2bb21048171beca5057a53033fd45d
Unknown <ranjurave@gmail.com>
2019-02-19 16:09:44 +1300

    oscillator

---
+++ Assets/Oscillator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f;
+    [Range(0, 1)] [SerializeField] float movementFactor;
+    Vector3 startingPos;
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+    
+    void Update()
+    {
+        float cycles = Time.time / period; // grows continually from 0
+        const float tau = Mathf.PI * 2; // about 6.28
+        float rawSinWave = Mathf.Sin(cycles * tau);// goes from -1 to +1
+        movementFactor = rawSinWave / 2f + 0.5f;
+
+        Vector3 offset = movementFactor * movementVector;
+        transform.position = offset + startingPos;
+    }
+}
